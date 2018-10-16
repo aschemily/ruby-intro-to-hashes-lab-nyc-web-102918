0a785b92d7fb60c2f6c233b155afb87e20691d97
Emily Asch <e.asch@me.com>
2018-10-16 18:05:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ end
 # 	    pennsylvania_railroad: {"mortgage_value" => "$200"},
 # 	    b_and_o_railroad: {"mortgage_value" => "$400"},
 # 	    shortline: {"mortgage_value" => "$800"},
-# 	  }
+# 	  },
 # 	    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned:50, three_pieces_ownded:100, four_pieces_owned: 200}
 # 	  }
 # 	}
