77f14d242eef76153fc53b5235a3c44a8889ee98
Emily Asch <e.asch@me.com>
2018-10-16 18:00:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
 	    b_and_o_railroad: {},
 	    shortline: {},
 	  }
-	    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned:50, three_pieces_ownded:100, }
+	    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned:50, three_pieces_ownded:100, four_pieces_owned: 400}
 	  }
 	}
 end
